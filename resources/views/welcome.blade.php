b0bdacd82a67bcf63b788b68b442638db7484750
thinkverse <hallberg.kim@gmail.com>
2022-09-13 10:09:42 +0200

    Add Livewire assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        @livewireStyles
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -128,5 +130,7 @@
                 </div>
             </div>
         </div>
+
+        @livewireScripts
     </body>
 </html>
