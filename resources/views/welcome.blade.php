bffaac7fc29573fa5df0ca3f223b086f78a5f1d6
thinkverse <hallberg.kim@gmail.com>
2022-09-13 10:51:05 +0200

    Update Vite directive to include `app.js`

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
     </head>
